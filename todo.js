94c6393ccc66023e37b66856cab8754f414a5e7a
aeligzandra <alexmary554@gmail.com>
2023-02-10 20:57:56 +0100

    first commit

---
+++ todo.js
@@ -0,0 +1,104 @@
+let form = document.getElementById('form');
+let textInput = document.getElementById('textInput');
+let dateInput = document.getElementById('dateInput');
+let textarea = document.getElementById('textarea');
+let msg = document.getElementById('msg');
+let datemsg = document.getElementById('datemsg');
+let descpmsg = document.getElementById('descpmsg');
+let tasks = document.getElementById('tasks');
+let add = document.getElementById('add');
+let data = [];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  formValidation();
+});
+
+let formValidation = () => {
+  if (textInput.value === '') {
+    console.log('failure');
+    msg.innerHTML = 'Title cannot be blank';
+  }
+  if (dateInput.value === '') {
+    console.log('failure');
+    datemsg.innerHTML = 'Date cannot be blank';
+  }
+  if (textarea.value === '') {
+    console.log('failure');
+    descpmsg.innerHTML = 'Description cannot be blank';
+  } else {
+    console.log('success');
+    msg.style.color = 'green';
+    msg.innerHTML = 'success';
+    datemsg.innerHTML = '';
+    descpmsg.innerHTML = '';
+    acceptData();
+  }
+};
+
+let acceptData = () => {
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    description: textarea.value,
+  });
+  localStorage.setItem('data', JSON.stringify(data));
+  console.log(data);
+
+  add.setAttribute('data-bs-dismiss', 'modal');
+  add.click();
+
+  (() => {
+    add.setAttribute('data-bs-dismiss', '');
+  })();
+
+  createTasks();
+};
+
+let createTasks = () => {
+
+  tasks.innerHTML = '';
+  data.map((x, y) => {
+    return (tasks.innerHTML += `
+	<div id=${y}>
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${x.date}</span>
+          <p>${x.description}</p>
+  
+          <span class="options">
+            <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
+          </span>
+        </div>
+`);
+  });
+  resetForm();
+};
+
+let resetForm = () => {
+  textInput.value = '';
+  dateInput.value = '';
+  textarea.value = '';
+};
+
+let deleteTask = (e) => {
+  e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+
+  localStorage.setItem('data', JSON.stringify(data));
+};
+let editTask = (e) => {
+  let selectedTasks = e.parentElement.parentElement;
+
+  textInput.value = selectedTasks.children[0].innerHTML;
+  dateInput.value = selectedTasks.children[1].innerHTML;
+  textarea.value = selectedTasks.children[2].innerHTML;
+
+  deleteTask(e);
+};
+
+(()=>{
+ data = JSON.parse(localStorage.getItem('data')) || [];
+  console.log(data);
+createTasks()
+})();
